3bc4082c6108f471619476d538b5fae44ef30bc1
KseniaKuzmina <ksenya.kuzmina83@gmail.com>
2023-04-16 20:39:15 +0300

    Tasks HW7 added

---
+++ Task2/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int Read(string arg)
+{
+    System.Console.Write($"введите {arg}: ");
+    int result=int.Parse(Console.ReadLine());
+    return result;
+}
+
+
+void FindNumber(int index1, int index2, int [,] Array)
+{   
+    if (index1 > Array.GetLength(0) || index2 > Array.GetLength(1))
+    {
+        System.Console.WriteLine("такого числа в массиве нет!");
+    }
+    else
+    {int number = Array[index1,index2];
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+           if(i==index1 && j==index2)
+           {
+            number=Array[i,j];
+           }
+                                        
+        }
+    }
+    System.Console.WriteLine($"значение элемента - {number}");
+    }
+
+}
+
+int [,] CreateArray (int length1, int lenght2)
+{
+    int [,] Array = new int [length1, lenght2];
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+           Array[i,j] = new Random().Next(0, 10);
+        }
+    }
+    return Array;
+}
+
+void PrintArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+           System.Console.Write(Array[i,j]+" ");
+           
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int index1 = Read($"индекс 1");
+int index2= Read($"индекс 2");
+int [,] array = CreateArray(4,4);
+PrintArray(array);
+FindNumber(index1, index2, array);
---
+++ Task3/Program.cs
@@ -0,0 +1,79 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void GetAverage0(int[,] Array)
+{
+    double Sum = 0;
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        Sum = Sum + Array[i, 0];
+    }
+    System.Console.Write("Среднее арифметическое каждого столбца: " + Math.Round((Sum/3),1));
+}
+
+void GetAverage1(int[,] Array)
+{
+    double Sum = 0;
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        Sum = Sum + Array[i, 1];
+    }
+    System.Console.Write(" , " + Math.Round((Sum/3),1));
+}
+
+void GetAverage2(int[,] Array)
+{
+    double Sum = 0;
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        Sum = Sum + Array[i, 2];
+    }
+    System.Console.Write(",  " + Math.Round((Sum/3),1));
+}
+
+void GetAverage3(int[,] Array)
+{
+    double Sum = 0;
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        Sum = Sum + Array[i, 3];
+    }
+    System.Console.Write(",  " + Math.Round((Sum/3),1));
+}
+
+int [,] CreateArray (int length1, int lenght2)
+{
+    int [,] Array = new int [length1, lenght2];
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+           Array[i,j] = new Random().Next(0, 10);
+        }
+    }
+    return Array;
+}
+
+void PrintArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+           System.Console.Write(Array[i,j]+" ");
+           
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int [,] array = CreateArray(3,4);
+PrintArray(array);
+GetAverage0(array);
+GetAverage1(array);
+GetAverage2(array);
+GetAverage3(array);
